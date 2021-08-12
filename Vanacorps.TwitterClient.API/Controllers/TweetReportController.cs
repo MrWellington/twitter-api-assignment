8f137212e96e657b287c3cdfebb47e3e7375af74
JJ Vanacore <jvanacore@aspenti.com>
2021-08-12 11:33:47 -0400

    Add exception handling to API call

--- Vanacorps.TwitterClient.API/Controllers/TweetReportController.cs
+++ Vanacorps.TwitterClient.API/Controllers/TweetReportController.cs
@@ -46,33 +46,61 @@ namespace Vanacorps.TwitterClient.API.Controllers
         }
 
         [HttpGet]
-        public async Task<TweetReportDto> Get()
+        public async Task<dynamic> Get()
         {
-            var reportQueries = new List<Task>
-            {
-                _averageTweetQuery.QueryAsync(),
-                _emojiPercentQuery.QueryAsync(),
-                _photoUrlPercentQuery.QueryAsync(),
-                _topDomainsQuery.QueryAsync(),
-                _topEmojisQuery.QueryAsync(),
-                _topHashtagsQuery.QueryAsync(),
-                _totalTweetQuery.QueryAsync(),
-                _urlPercentQuery.QueryAsync()
-            };
+            _logger.LogDebug("Generating tweet report via API");
 
-            await Task.WhenAll(reportQueries);
-
-            return new TweetReportDto
+            try
             {
-                TweetAverages = ((Task<TweetAveragesDto>)reportQueries[0]).Result,
-                PercentWithEmojis = ((Task<decimal>)reportQueries[1]).Result,
-                PercentWithPhotoUrls = ((Task<decimal>)reportQueries[2]).Result,
-                TopDomains = ((Task<List<TopDomains>>)reportQueries[3]).Result,
-                TopEmojis = ((Task<List<TopEmojis>>)reportQueries[4]).Result,
-                TopHashtags = ((Task<List<TopHashtags>>)reportQueries[5]).Result,
-                TotalTweetCount = ((Task<int>)reportQueries[6]).Result,
-                PercentWithUrls = ((Task<decimal>)reportQueries[7]).Result,
-            };
+                var reportQueries = new List<Task>
+                {
+                    _averageTweetQuery.QueryAsync(),
+                    _emojiPercentQuery.QueryAsync(),
+                    _photoUrlPercentQuery.QueryAsync(),
+                    _topDomainsQuery.QueryAsync(),
+                    _topEmojisQuery.QueryAsync(),
+                    _topHashtagsQuery.QueryAsync(),
+                    _totalTweetQuery.QueryAsync(),
+                    _urlPercentQuery.QueryAsync()
+                };
+
+                await Task.WhenAll(reportQueries);
+
+                return new TweetReportDto
+                {
+                    TweetAverages = ((Task<TweetAveragesDto>)reportQueries[0]).Result,
+                    PercentWithEmojis = ((Task<decimal>)reportQueries[1]).Result,
+                    PercentWithPhotoUrls = ((Task<decimal>)reportQueries[2]).Result,
+                    TopDomains = ((Task<List<TopDomains>>)reportQueries[3]).Result,
+                    TopEmojis = ((Task<List<TopEmojis>>)reportQueries[4]).Result,
+                    TopHashtags = ((Task<List<TopHashtags>>)reportQueries[5]).Result,
+                    TotalTweetCount = ((Task<int>)reportQueries[6]).Result,
+                    PercentWithUrls = ((Task<decimal>)reportQueries[7]).Result,
+                };
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.Flatten().InnerExceptions)
+                {
+                    _logger.LogError(e, "Error occurred generating tweet report: ");
+                }
+
+                return new
+                {
+                    ErrorOccurred = true,
+                    Message = "Error occurred generating tweet report...check logs for more details."
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred generating tweet report: ");
+
+                return new
+                {
+                    ErrorOccurred = true,
+                    Message = "Error occurred generating tweet report...check logs for more details."
+                };
+            }
         }
     }
 }
